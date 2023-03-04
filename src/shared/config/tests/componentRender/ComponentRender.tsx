e5e52ebe0acd649d0c5f07b4975710d115cbb82d
Viacheslav Orlov <viacheslavorlov@gmail.com>
2023-03-04 22:49:51 +0300

    модуль 4 урок 40: починка после STRICT MODE

--- src/shared/config/tests/componentRender/ComponentRender.tsx
+++ src/shared/config/tests/componentRender/ComponentRender.tsx
@@ -17,12 +17,12 @@ export function ComponentRender(component: ReactNode, options: RenderWithRouterO
     } = options;
 
     return render(
-        <StoreProvider initialState={initialState}>
-            <MemoryRouter initialEntries={[route]}>
+        <MemoryRouter initialEntries={[route]}>
+            <StoreProvider initialState={initialState}>
                 <I18nextProvider i18n={i18n}>
                     {component}
                 </I18nextProvider>
-            </MemoryRouter>
-        </StoreProvider>,
+            </StoreProvider>
+        </MemoryRouter>,
     );
 }
