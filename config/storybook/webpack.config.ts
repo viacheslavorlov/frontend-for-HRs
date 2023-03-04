231bcd883af84d4f9309e4f826843a39a6bbf19e
Viacheslav Orlov <viacheslavorlov@gmail.com>
2023-03-04 23:19:20 +0300

    модуль 4 урок 40: починка после STRICT MODE storybook упал и не дышит!

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -3,26 +3,27 @@ import path from 'path';
 import { buildCssLoader } from '../build/cssLoaders/buldCSSLoader';
 
 export default ({ config }: { config: webpack.Configuration }) => {
-    config.resolve.modules.push(path.resolve(__dirname, '..', '..', 'src'));
+    config!.resolve!.modules!.push(path.resolve(__dirname, '..', '..', 'src'));
 
-    config.resolve.extensions.push('ts', 'tsx');
+    config!.resolve!.extensions!.push('ts', 'tsx');
 
-    config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
+    // @ts-ignore
+    config!.module!.rules! = config.module!.rules!.map((rule: RuleSetRule) => {
         if (/svg/.test(rule.test as string)) {
             return { ...rule, exclude: /\.svg$/i };
         }
         return rule;
     });
 
-    config.module.rules.push({
+    config!.module!.rules!.push({
         test: /\.svg$/i,
         issuer: /\.[jt]sx?$/,
         use: ['@svgr/webpack'],
     });
 
-    config.module.rules.push(buildCssLoader(true));
+    config!.module!.rules!.push(buildCssLoader(true));
 
-    config.plugins.push(new DefinePlugin({
+    config!.plugins!.push(new DefinePlugin({
         __IS_DEV: true,
         __API_URL: '',
     }));
