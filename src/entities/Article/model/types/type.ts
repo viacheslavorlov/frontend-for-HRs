a0c1312601bbf470f7ee645c00be16833a8aff8b
viacheslav <viacheslavorlov@gmail.com>
2023-03-16 20:10:31 +0300

    модуль 7: урок 49 -  12мин

---
+++ src/entities/Article/model/types/type.ts
@@ -0,0 +1,46 @@
+export enum ArticleType {
+    IT = 'IT',
+    SCIENCE = 'SCIENS',
+    ECONOMICS = 'ECONOMICS'
+}
+
+export enum ArticleBlockType {
+    IMAGE = 'IMAGE',
+    TEXT = 'TEXT',
+    CODE = 'CODE'
+}
+
+export interface ArticleBase {
+    id: string;
+    type: ArticleBlockType;
+}
+
+export interface ArticleCodeBlock extends ArticleBase{
+    type: ArticleBlockType.CODE;
+    code: string;
+}
+
+export interface ArticleTextBlock extends ArticleBase {
+    type: ArticleBlockType.TEXT;
+    title?: string;
+    paragraphs: string[];
+}
+
+export interface ArticleImageBlock extends ArticleBase {
+    type: ArticleBlockType.IMAGE;
+    src: string;
+    title: string;
+}
+
+export type ArticleBlock = ArticleCodeBlock | ArticleTextBlock | ArticleImageBlock;
+
+export interface Article {
+    id: string;
+    title: string;
+    subtitle: string;
+    img: string;
+    views: number;
+    createdAt: string;
+    type: ArticleType[];
+    blocks: ArticleBlock[];
+}
