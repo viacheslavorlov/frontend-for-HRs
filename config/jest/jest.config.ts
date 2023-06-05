69cb36e59b07ed8799029cc7348b8db521f75fb7
Viacheslav Orlov <viacheslavorlov@gmail.com>
2023-06-05 14:32:18 +0300

    main.yml update visual test generated changed to chrome.docker tests fixed fo linux

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -39,6 +39,8 @@ export default {
         '\\.s?css$': 'identity-obj-proxy',
         '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
         '^@/(.*)$': '<rootDir>/src/$1',
+        '^axios$': require.resolve('axios'),
+
     },
     reporters: [
         'default',
