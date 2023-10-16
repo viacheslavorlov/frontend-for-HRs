2e0fe92adae325f7f8289a6d93531ebe94aa0f2d
Orlov Viacheslav <viacheslavorlov@gmail.com>
2023-10-16 12:23:33 +0300

    redesign  started main branch fixed

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,57 +8,39 @@ import { Suspense, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { AppRouter } from './router';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
-import { ToggleFeature } from '@/shared/lib/features/ToggleFeature/ToggleFeature';
 
 function App() {
-    const { theme } = useTheme();
-    const dispatch = useAppDispatch();
-    const inited = useSelector(getUserInited);
-
-    useEffect(() => {
-        dispatch(initAuthData());
-    }, [dispatch]);
-
-    if (!inited) {
-        return (
-            <div className={classNames('app', {}, [theme])}>
-                <PageLoader />
-            </div>
-        );
-    }
-
-    return (
-        <ToggleFeature
-            feature={'isNewDesign'}
-            on={<div className={classNames('app_redesigned', {}, [theme])}>
-                <Suspense fallback={<PageLoader />}>
-                    <Navbar />
-                    <div className='content-page'>
-                        <Sidebar />
-                        {inited && (
-                            <Suspense fallback={<PageLoader />}>
-                                <AppRouter />
-                            </Suspense>
-                        )}
-                    </div>
-                </Suspense>
-            </div>}
-            off={<div className={classNames('app', {}, [theme])}>
-                <Suspense fallback={<PageLoader />}>
-                    <Navbar />
-                    <div className='content-page'>
-                        <Sidebar />
-                        {inited && (
-                            <Suspense fallback={<PageLoader />}>
-                                <AppRouter />
-                            </Suspense>
-                        )}
-                    </div>
-                </Suspense>
-            </div>}
-            />
-
-    );
+	const { theme } = useTheme();
+	const dispatch = useAppDispatch();
+	const inited = useSelector(getUserInited);
+	
+	useEffect(() => {
+		dispatch(initAuthData());
+	}, [dispatch]);
+	
+	if (!inited) {
+		return (
+			<div className={classNames('app', {}, [theme])}>
+				<PageLoader />
+			</div>
+		);
+	}
+	
+	return (
+		<div className={classNames('app_redesigned', {}, [theme])}>
+			<Suspense fallback={<PageLoader />}>
+				<Navbar />
+				<div className='content-page'>
+					<Sidebar />
+					{inited && (
+						<Suspense fallback={<PageLoader />}>
+							<AppRouter />
+						</Suspense>
+					)}
+				</div>
+			</Suspense>
+		</div>
+	);
 }
 
 export default App;
