c8585047203006bc481c03c00186435d11f31fa5
Orlov Viacheslav <viacheslavorlov@gmail.com>
2023-10-16 12:23:33 +0300

    redesign  sidebar

---
+++ src/shared/layouts/MainLayout/MainLayout.tsx
@@ -0,0 +1,34 @@
+import cls from './MainLayout.module.scss';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import { ReactElement } from 'react';
+
+interface MainLayoutProps {
+	className?: string;
+	header: ReactElement;
+	content: ReactElement;
+	sidebar: ReactElement;
+	toolbar?: ReactElement;
+	
+}
+
+export const MainLayout = (props: MainLayoutProps) => {
+	const {
+		className,
+		content,
+		toolbar,
+		header,
+		sidebar,
+	} = props;
+	return (
+		<div className={classNames(cls.MainLayout, {}, [className])}>
+			<div className={cls.sidebar}>{sidebar}</div>
+			<div className={cls.content}>{content}</div>
+			<div className={cls.rightbar}>
+				<div className={cls.header}>{header}</div>
+				<div className={cls.toolbar}>{toolbar}</div>
+			</div>
+		
+		</div>
+	);
+};
+
---
+++ src/widgets/Sidebar/ui/Sidebar/NewSideBar.tsx
@@ -0,0 +1,68 @@
+import { memo, useMemo, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { ThemeSwitcher } from '@/features/ThemeSwitcher';
+import { TranslateSwitcher } from '@/features/TranslateSwitcher';
+import { Button, ButtonSize, ButtonTheme } from '@/shared/ui/Button';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import { VStack } from '@/shared/ui/Stack';
+import { getSidebarItems } from '../../model/selectors/getSidebarItems';
+import { SidebarItem } from '../SidebarItem/SidebarItem';
+import cls from './NewSidebar.module.scss';
+import { SIDEBAR_LOCAL_STORAGE_KEY } from '@/shared/const/localStorage/localStorage';
+
+interface SidebarProps {
+	className?: string;
+}
+
+export const NewSidebar = memo(({ className }: SidebarProps) => {
+	const [collapsed, setCollapsed] = useState<boolean>(
+		JSON.parse(localStorage.getItem(SIDEBAR_LOCAL_STORAGE_KEY) || 'false'),
+	);
+	const sidebarItemsList = useSelector(getSidebarItems);
+	const onToggle = () => {
+		localStorage.setItem(SIDEBAR_LOCAL_STORAGE_KEY, String(!collapsed));
+		setCollapsed((prevState) => !prevState);
+	};
+	const itemList = useMemo(
+		() =>
+			sidebarItemsList.map((item) => (
+				<SidebarItem
+					className={cls.item}
+					key={item.path}
+					item={item}
+					collapsed={collapsed}
+				/>
+			)),
+		[sidebarItemsList, collapsed],
+	);
+	
+	return (
+		<aside
+			data-testid="sidebar"
+			className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
+		>
+			<VStack
+				role="navigation"
+				gap="8"
+				align={collapsed ? 'center' : 'start'}
+				className={cls.items}
+			>
+				{itemList}
+			</VStack>
+			<Button
+				data-testid="sidebar-toggle"
+				onClick={onToggle}
+				className={cls.collapseBtn}
+				theme={ButtonTheme.BACKGROUND_INVERTED}
+				size={ButtonSize.XL}
+				square
+			>
+				{collapsed ? '>' : '<'}
+			</Button>
+			<div className={cls.switchers}>
+				<ThemeSwitcher />
+				<TranslateSwitcher short={collapsed} className={cls.lang} />
+			</div>
+		</aside>
+	);
+});
